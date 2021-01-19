ccddb46e9c3c495bbb7f8f0a0d7456bf25972f06
Jason Tomlins <jason.tomlins@gmail.com>
2021-01-19 12:01:35 +1000

    Added a 404 error page to display when non-existent page is requests to replace standard whitelabel page. Also, updated application properties to kill any open session between server restarts. Updated LoginController to redirect already logged in users back to home page.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    @GetMapping("/")
+    public String defaultLoginView() {
+        return loginView();
+    }
+
     @GetMapping("/login")
     public String loginView() {
-        return "login";
+
+        // Only direct user to login page if they are not already logged in
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || (authentication != null && authentication instanceof AnonymousAuthenticationToken)) {
+            return "login";
+        }
+
+        // Otherwise, redirect back home
+        return "redirect:/home";
     }
 
 }
